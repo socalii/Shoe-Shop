63d3984481cef96e2c3195be8ff0437e96601242
Susan Choi <2539lee@gmail.com>
2022-04-30 23:34:59 -0700

    Layout & Product Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap'
+import { react, useState } from 'react'
+import Data from './data.js'
 
 function App() {
+  const [shoes, setShoes] = useState(Data)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <Navbar bg='light' expand='lg'>
+        <Container>
+          <Navbar.Brand href='#home'>Shoes Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='me-auto'>
+              <Nav.Link href='#home'>ShoeShop</Nav.Link>
+              <Nav.Link href='#link'>Link</Nav.Link>
+              <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
+                <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
+                <NavDropdown.Item href='#action/3.2'>
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href='#action/3.3'>
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href='#action/3.4'>
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className='jumbotron background'>
+        <h1>20% Season OFF</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          <Button variant='primary'>Learn more</Button>
+        </p>
+      </div>
+
+      <div className='container'>
+        <div className='row'>
+          {shoes.map((a, i) => {
+            return <Card shoes={shoes[i]} i={i} />
+          })}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+function Card(props) {
+  return (
+    <div className='col-md-4'>
+      <img
+        src={
+          'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'
+        }
+        width='100%'
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
+      <p>{props.shoes.price}</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 0,
+    title: 'White and Black',
+    content: 'Born in France',
+    price: '$120',
+  },
+  {
+    id: 1,
+    title: 'Red Knit',
+    content: 'Born in Seoul',
+    price: '$130',
+  },
+  {
+    id: 0,
+    title: 'Grey Yordan',
+    content: 'Born in States',
+    price: '$110',
+  },
+]
